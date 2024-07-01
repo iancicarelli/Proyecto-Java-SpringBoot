a03f5e8b1db7e7a5692a8d304a0abbbd74f15f31
Nicoblessjah <nicoblessjah@gmail.com>
2024-07-01 17:58:12 -0400

    carpetas en java

--- SpringApp/SpringApp/src/main/java/com/zapatillas/controller/PageController.java
+++ SpringApp/SpringApp/src/main/java/com/zapatillas/controller/PageController.java
@@ -1,9 +1,10 @@
 package com.zapatillas.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ControladorPagina {
+public class PageController {
 
     @GetMapping("/login")
     public String login() {
